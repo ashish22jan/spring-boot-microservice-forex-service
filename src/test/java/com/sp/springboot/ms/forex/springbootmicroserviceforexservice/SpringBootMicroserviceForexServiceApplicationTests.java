33af62fb3f2df7acf0878bd980cea8acd532a502
aramtri <aramtri@WIN-8KQPF0E2BFB.in.oracle.com>
2020-07-21 21:03:16 +0530

    Commit 1

---
+++ src/test/java/com/sp/springboot/ms/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sp.springboot.ms.forex.springbootmicroserviceforexservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBootMicroserviceForexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
